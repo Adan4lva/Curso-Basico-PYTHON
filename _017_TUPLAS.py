9943a47d91318193c4fea4c778b61f207010dc5e
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 13:44:57 -0600

    Add files via upload

---
+++ _017_TUPLAS.py
@@ -0,0 +1,19 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print("\n\n", colores[1], "se encuentra en la 2° posicion de la tupla")
+
+numeros = (10, 1, 5, 11)
+operacion = numeros[0] + numeros[2] + numeros[3] - numeros[1]
+
+print("\nEl resultado de la operacion es: ", operacion)
+
+
+print("\n\n")
