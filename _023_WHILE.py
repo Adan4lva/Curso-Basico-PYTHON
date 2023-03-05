d473396a41dabc8df7f2e79df3bbedc800f62da5
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 14:57:23 -0600

    Add files via upload

---
+++ _023_WHILE.py
@@ -0,0 +1,27 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+x = 0
+print("Primer While")
+while x <= 15:
+	print(x)
+	x += 5
+
+x = 0
+print("\nSegundo While")
+while x >= -100:
+	print(x)
+	x -= 20
+
+x = 10
+print("\nTercer While")
+while x >= 0:
+	print('El valor de x es: ', x)
+	x -= 1
+
+print("\n\n")
