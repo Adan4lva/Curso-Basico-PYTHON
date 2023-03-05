ea1af5debf40082f6a9e418802f007f991e61976
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 01:11:53 -0600

    Add files via upload

---
+++ _008_FLOATROUND.py
@@ -0,0 +1,20 @@
+"""
+
+Created on fri 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+
+numero = 17.567383292929200234
+
+print("\nNumero decimal: ")
+print(numero)
+
+numero = round(17.567383292929200234, 5)
+
+print("\n\nNumero redondeado: ")
+print(numero)
+
+print("\n\n")
