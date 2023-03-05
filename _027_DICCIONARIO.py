6e50a038695bd8afe013c464f9917bc18714920a
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 16:15:34 -0600

    Add files via upload

---
+++ _027_DICCIONARIO.py
@@ -0,0 +1,23 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print('\nEl modelo', teclado2['Modelo'], 'cuesta', teclado2['Precio'], '$.')
+
+print("\n\n")
