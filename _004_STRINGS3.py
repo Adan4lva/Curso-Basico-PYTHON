ca66dbf22fc30336dfef20268b7d719c6c83814c
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 00:49:13 -0600

    Add files via upload

---
+++ _004_STRINGS3.py
@@ -0,0 +1,23 @@
+"""
+
+Created on fri 3 March 2023
+
+By Adan Alvarez
+
+"""
+
+import time
+
+nombre = "adan alvarez barajas"
+
+print("\n" + nombre.title() + "   (Primeras Mayusculas)")
+
+time.sleep(0.5)
+
+print("\n" + nombre.upper() + "   (MAYUSCULAS)")
+
+time.sleep(0.5)
+
+print("\n" + nombre.lower() + "   (minusculas)" + "\n\n")
+
+
