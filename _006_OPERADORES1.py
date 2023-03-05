1a5b868f057cb52e036b40faae3ebd06e6887c09
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 00:55:13 -0600

    Add files via upload

---
+++ _006_OPERADORES1.py
@@ -0,0 +1,35 @@
+"""
+
+Created on fri 3 March 2023
+
+By Adan Alvarez
+
+"""
+
+import time
+
+sum = 20 + 23 + 44
+print("\n20 + 23 + 44 =")
+print(sum)
+time.sleep(0.5)
+
+res = 20 - 23 - 84
+print("\n20 - 23 - 84 =")
+print(res)
+time.sleep(0.5)
+
+multi = 10 * 87
+print("\n10 * 87 =")
+print(multi)
+time.sleep(0.5)
+
+div = 5000 / 230 / 2
+print("\n5000 / 230 / 2 =")
+print(div)
+time.sleep(0.5)
+
+operacion = 10 / 5 + 15 - 17
+print("\n10 / 5 + 15 - 17 =")
+print(operacion)
+
+print("\n\n")
