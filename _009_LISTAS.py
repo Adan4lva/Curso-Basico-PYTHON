c0c04ad430d96ec72dddcc7345935c14b9b9e552
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 01:29:48 -0600

    Add files via upload

---
+++ _009_LISTAS.py
@@ -0,0 +1,22 @@
+"""
+
+Created on sat 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+import time
+
+colores = ["Rojo", "Azul", "Verde", "Amarillo", "Marrón", "Lila", "Negro", "Rosa"]
+
+print("\n" + colores[3] + "   se encuentra en la posicion 3")
+time.sleep(0.5)
+print(colores[0] + "       se encuentra en la posicion 0")
+time.sleep(0.5)
+print(colores[7] + "       se encuentra en la posicion 7\n\n")
+time.sleep(0.5)
+
+numeros = ["tres, ", "dos, ", "cinco, ", "cuatro, ", "uno]"]
+
+print("Numeros = [" + numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4] + "\n\n")
