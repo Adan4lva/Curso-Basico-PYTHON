1507fc6f8aee08bab328ce6bd069ab843142f326
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 13:52:01 -0600

    Add files via upload

---
+++ _018_CONVERTIRTUPLAS.py
@@ -0,0 +1,16 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colores_tupla = tuple(colores)
+
+print("\n", colores_tupla)
+print("\nTipo de elemento: ", type(colores_tupla), "\n\n")
+
