7a6b736d53e090aeef32d99cb6075f64f7c987ae
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 16:23:52 -0600

    Add files via upload

---
+++ _028_DICCIONARIOFOR.py
@@ -0,0 +1,18 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+for x in teclado1:
+	print(x, '=', teclado1[x] + '.')
+
+print("\n\n")
