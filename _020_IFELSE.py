576ea3ed099be58168d929e87a21ffee19864846
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 14:26:05 -0600

    Add files via upload

---
+++ _020_IFELSE.py
@@ -0,0 +1,17 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+color = "rojo"
+
+
+if color == "rojo":
+    print("\nEl color es rojo!\n\n")
+
+else:
+    print("\nEl color NO es rojo!\n\n")
+
