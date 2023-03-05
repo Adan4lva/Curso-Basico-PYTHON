e34a901227bbe824187ad02b1ac54e20a9083dda
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 12:54:10 -0600

    Add files via upload

---
+++ _014_LISTAS6.py
@@ -0,0 +1,27 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+import time
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print("\nLista original:")
+
+for elemento in colores:
+    time.sleep(0.2)
+    print(elemento)
+
+colores.append('fuxia')
+colores.append('celeste')
+
+
+print("\nLista modificada:")
+for elemento in colores:
+    time.sleep(0.2)
+    print(elemento)
+
+print("\n\n")
