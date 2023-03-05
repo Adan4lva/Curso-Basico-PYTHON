8fe4a89e7e9425fcc1937351642115367cb12975
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 14:07:22 -0600

    Add files via upload

---
+++ _019_IF.py
@@ -0,0 +1,29 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+	print('15 < 20    → Se ejecuta el if.')
+
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('1450 > 60  → Se ejecuta el if.')
+
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('60 != 60   → Se ejecuta el if.')
+
+print('60 != 60   → NO se ejecuta el if.')
+
+print("\n\n")
