ce05671062c408036ba68406215bde5badc59450
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 15:24:51 -0600

    Add files via upload

---
+++ _025_FOR.py
@@ -0,0 +1,19 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+import time
+
+colores = ('rojo', 'azul', 'verde', 'amarillo')
+
+print("\n")
+
+for x in colores:
+	print('El color es: ' + x + '.')
+	time.sleep(0.5)
+
+print("\n\n")
