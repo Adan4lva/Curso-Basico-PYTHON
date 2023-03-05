7d4e081cb71c39e96ba5dd57e54f1d437da33440
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-05 13:11:44 -0600

    Add files via upload

---
+++ _016_LISTAS8.py
@@ -0,0 +1,26 @@
+"""
+
+Created on sun 5 March 2023
+
+By Adan Alvarez
+
+"""
+
+import time
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print("\nLista original:")
+
+for elemento in colores:
+    time.sleep(0.2)
+    print(elemento)
+
+colores.sort(reverse=True)
+
+print("\nLista modificada:")
+for elemento in colores:
+    time.sleep(0.2)
+    print(elemento)
+
+print("\n\n")
+
