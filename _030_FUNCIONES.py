ddaa1a83100445617850f94deb989633f8d69cfe
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-10 00:27:34 -0600

    Add files via upload

---
+++ _030_FUNCIONES.py
@@ -0,0 +1,16 @@
+"""
+
+Created on thu 9 March 2023
+
+By Adan Alvarez
+
+"""
+
+def suma(numero1, numero2):
+	print("\n", numero1, " + ", numero2, " = ", numero1 + numero2)
+
+suma(10, 20)
+suma(20, 30)
+suma(50000, 7000)
+
+print("\n")
