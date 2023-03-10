4ee916ff891760cff607293bba317805a72f49bd
Adan Alvarez <a20310442@live.ceti.mx>
2023-03-10 00:19:13 -0600

    Add files via upload

---
+++ _029_METODODICC.py
@@ -0,0 +1,23 @@
+"""
+
+Created on thu 9 March 2023
+
+By Adan Alvarez
+
+"""
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+del teclado1
+del teclado2['Categoría']
+del teclado2['Precio']
+print("\n", teclado2['Modelo'], "\n\n")
